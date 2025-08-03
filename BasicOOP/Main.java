e89c2b2f36583b6bb1aee8268e2af282580d6bfe
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2025-08-03 18:37:10 +0530

    Create Main.java

---
+++ BasicOOP/Main.java
@@ -0,0 +1,35 @@
+/*Design a class Student with the following:
+Fields: name, age, grade.
+Methods:
+A constructor to initialize all fields.
+A method displayDetails() to print the student's details.
+Create 3 student objects and call displayDetails() for each.*/
+
+package com.pkg;
+
+class Student {
+	String name;
+	int age;
+	char grade;
+
+	public Student(String name, int age, char grade) {
+		this.name = name;
+		this.age = age;
+		this.grade = grade;
+	}
+
+	void displayDetails() {
+		System.out.println("Name : " + name + " Age : " + age + " Grade : " + grade);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Student s1 = new Student("Adam", 24, 'A');
+		Student s2 = new Student("Jack", 42, 'B');
+		Student s3 = new Student("Richard", 34, 'C');
+		s1.displayDetails();
+		s2.displayDetails();
+		s3.displayDetails();
+	}
+}
