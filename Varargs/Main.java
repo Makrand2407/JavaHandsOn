41e7913ddbbec0b887ddd0950019f33685e412ad
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2025-08-03 18:49:04 +0530

    Update Main.java

--- Varargs/Main.java
+++ Varargs/Main.java
@@ -1,7 +1,3 @@
-/*Write a method calculateSum(int... numbers) that:
-Accepts a variable number of integers.
-Calculates and prints their sum.
-Test the method with different numbers of arguments.*/
 
 package com.pkg;
 
