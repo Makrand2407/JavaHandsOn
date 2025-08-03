bba8da02fec61da765c58f02b3f986438584efa3
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2025-08-03 18:41:46 +0530

    Create Main.java

---
+++ Varargs/Main.java
@@ -0,0 +1,33 @@
+/*Write a method calculateSum(int... numbers) that:
+Accepts a variable number of integers.
+Calculates and prints their sum.
+Test the method with different numbers of arguments.*/
+
+package com.pkg;
+
+class Print {
+	void calculateSum(int... numbers) {
+		int sum = 0;
+		for (int s : numbers) {
+			sum += s;
+		}
+		System.out.println("Sum : " + sum);
+	}
+
+	void calculateSum(double d, int... numbers) {
+		System.out.println(d);
+		int sum = 0;
+		for (int s : numbers) {
+			sum += s;
+		}
+		System.out.println("Sum : " + sum);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Print p = new Print();
+		p.calculateSum(1, 2, 3, 4);
+		p.calculateSum(2.4, 4, 6, 8);
+	}
+}
