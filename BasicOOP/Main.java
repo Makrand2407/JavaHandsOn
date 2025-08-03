40b2f03efa2820357285e12526a528936555f6d3
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2025-08-03 18:47:32 +0530

    Update Main.java

--- BasicOOP/Main.java
+++ BasicOOP/Main.java
@@ -1,9 +1,3 @@
-/*Design a class Student with the following:
-Fields: name, age, grade.
-Methods:
-A constructor to initialize all fields.
-A method displayDetails() to print the student's details.
-Create 3 student objects and call displayDetails() for each.*/
 
 package com.pkg;
 
