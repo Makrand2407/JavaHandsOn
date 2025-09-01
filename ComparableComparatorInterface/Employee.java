4f57e7fe4d4996f904eb1e5eb1b23a1b32331265
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2025-09-01 14:42:38 +0530

    Create Employee.java

---
+++ ComparableComparatorInterface/Employee.java
@@ -0,0 +1,39 @@
+package com.pkg;
+
+public class Employee {
+	Integer empId;
+	 String name;
+	 Double salary;
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Employee(Integer empId, String name, Double salary) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
