e2cb7270332685a68370576b6be9782775fc0aca
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2026-05-27 17:01:07 +0530

    Updated Main.java
    Added Comments

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -1,24 +1,32 @@
 package com.pkg;
 
+// Base class representing a generic Animal
 class Animal {
+	// Method to make a generic animal sound
 	void makeSound() {
 		System.out.println("Animals Sound");
 	}
 }
 
+// Subclass representing a Cat
 class Cat extends Animal {
+	// Overriding makeSound to provide Cat-specific behavior
 	void makeSound() {
 		System.out.println("Cat is meowing");
 	}
 }
 
+// Subclass representing a Dog
 class Dog extends Animal {
+   // Overriding makeSound to provide Dog-specific behavior
 	void makeSound() {
 		System.out.println("Dog is barking");
 	}
 }
 
+// Subclass representing a Cow
 class Cow extends Animal {
+  // Overriding makeSound to provide Cow-specific behavior
 	void makeSound() {
 		System.out.println("Cow is mooing");
 	}
@@ -26,10 +34,13 @@ class Cow extends Animal {
 
 public class Main {
 	public static void main(String[] args) {
+		// Animal reference pointing to Cat object
 		Animal a = new Cat();
 		a.makeSound();
+		// Animal reference pointing to Dog object
 		a = new Dog();
 		a.makeSound();
+		// Animal reference pointing to Cow object
 		a = new Cow();
 		a.makeSound();
 	}
