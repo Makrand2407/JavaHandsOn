e9fd2fb4277e5e28aaab8324c23f8f90b9daf8bf
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2026-05-28 14:44:57 +0530

    Update Main.java

--- Nested Class/NonStaticNested/Main.java
+++ Nested Class/NonStaticNested/Main.java
@@ -1,1 +1,21 @@
-
+package com.pkg;
+// Outer class
+public class Outer {
+	String message = "Hello"; // field in Outer class
+  // Inner class
+	class Inner{
+		int a = 10; // field in Inner class
+    
+    // Method to display values from both Inner and Outer
+		void display() {
+			System.out.println("a ="+a);
+			System.out.println("Outer="+message);
+		}
+	
+	}
+	public static void main(String[] args) {
+		Outer outer = new Outer();   // instance of Outer class
+		Outer.Inner inner = outer.new Inner(); // instance of Inner class using Outer object
+		inner.display();
+	}
+}
