a437bda2c874ba0faa84496f1dff8b0b0e4d02a2
Makrand Zade <74808361+Makrand2407@users.noreply.github.com>
2026-05-28 14:51:25 +0530

    Update Main.java

--- Nested Class/StaticNestedClass/Main.java
+++ Nested Class/StaticNestedClass/Main.java
@@ -1,1 +1,27 @@
 
+package com.pkg;
+
+//Outer class 
+public class Outer {
+	static int outer = 20; // static field in Outer class
+  
+  //Inner class
+	static class Inner{
+		int y = 10;  //Field in Inner class
+    
+    // Method to display values from both Outer and Inner
+		void display() {
+			System.out.println("Outer = "+outer);
+			System.out.println("y = "+y);
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Outer.Inner o = new Outer.Inner(); //Instance of Inner class directly using Outer.Inner
+		o.display();
+		
+
+	}
+
+}
